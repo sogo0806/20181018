81798d6ee4e4d7da3adec274083ed796f81c6fcb
20180403 <32056048+sogo0806@users.noreply.github.com>
2018-10-25 11:59:38 +0800

    Update FrameMain6.java

--- FrameMain6.java
+++ FrameMain6.java
@@ -1,6 +1,10 @@
 import javax.print.attribute.standard.JobKOctetsProcessed;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
 
 public class FrameMain6 extends JFrame {
     private JLabel jlb = new JLabel("加密法");
@@ -23,12 +27,22 @@ public class FrameMain6 extends JFrame {
     private JPanel jpne = new JPanel(new GridLayout(2,1,3,3));
     private JPanel jpnw = new JPanel(new GridLayout(2,1,3,3));
     private JPanel jpns = new JPanel(new GridLayout(1,1,3,3));
-    public FrameMain6 (){
+    FrameMain3 fd;
+    private JFileChooser jch = new JFileChooser();
+    private String loodFile;
+    public FrameMain6 (FrameMain3 fa){
         init();
+        fd = fa;
     }
     public void init(){
         this.setBounds(100,100,500,300);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                fd.setVisible(true);
+            }
+        });
         this.setLayout(null);
         cp = this.getContentPane();
         cp.setLayout(new BorderLayout());
@@ -50,6 +64,60 @@ public class FrameMain6 extends JFrame {
         jpne.add(jbtn3);
         jpne.add(jbtn4);
         jpns.add(jpb);
+
+
+        jbtn3.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (jch.showOpenDialog(FrameMain6.this) == JFileChooser.APPROVE_OPTION){
+//                    loodFile = jch.getSelectedFile().getPath() + jch.getSelectedFile().getName();
+                    jtf2.setText(jch.getSelectedFile().getPath());
+                    jtf3.setText(jtf2.getText()+".sec");
+                }
+            }
+        });
+
+        jbtn.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (jtf2.getText().equals(" ")) {
+                    JOptionPane.showMessageDialog(FrameMain6.this, "no file selected");
+                } else {
+                    try {
+                        File selected = new File(jtf2.getText());
+                        long length = selected.length();
+                        jpb.setMaximum(100);
+                        char key[] = jtf.getText().toCharArray();
+                        FileReader fr = new FileReader(selected);
+                        BufferedReader bf = new BufferedReader(fr);
+                        File writeFile = new File(jtf3.getText());
+                        FileWriter fw = new FileWriter(writeFile);
+                        BufferedWriter bw = new BufferedWriter(fw);
+                        int data;
+                        int i = 0;
+                        while ((data = bf.read()) != -1) {
+                            data = data ^ key[i & key.length];
+                            bw.write(data);
+                            i++;
+                            jpb.setValue(Math.round(((float) i / length) * 100));
+                            bf.close();
+                            fr.close();
+                            JOptionPane.showMessageDialog(FrameMain6.this, "Finish!");
+                        }
+                    } catch (IOException ie) {
+                        JOptionPane.showMessageDialog(FrameMain6.this, "no File");
+                    }
+
+
+                }
+
+
+            }
+
+        });
+
+
+
 
     }
 }
